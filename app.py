e7d3e5618e14134b97778556205992274c5a2394
EC2 Default User <ec2-user@ip-172-30-0-49.ap-south-1.compute.internal>
2026-03-27 21:57:57 +0000

    update homepage

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Agile Pipeline Success!</h1><p>Managed by Dinesh Kumar</p>'
+    return '<h1>Agile Pipeline Success!</h1><p>Managed by Dinesh Kumar Anthale</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
